a3595d1cadc0856f782e2ae7118792e82a0e1505
RizDas <dkkrishabh@gmail.com>
2026-01-01 18:17:43 +0530

    centering fixed

--- src/components/home/hero.tsx
+++ src/components/home/hero.tsx
@@ -7,39 +7,41 @@ type HeroProps = React.HTMLAttributes<HTMLElement>;
 
 const Hero = ({ className, ...props }: HeroProps) => {
   return (
-    <section {...props} className={`home ${className ?? ''}`}>
+    <section>
       <div className="circle-container">
         <div className="yellow-circle-top-left overflow-hidden" />
         <div className="yellow-circle-bottom-right overflow-hidden" />
       </div>
+      <section {...props} className={`home ${className ?? ''}`}>
+        <div className="container">
+          <div className="home-content">
+            <p className="top tag">
+              Tech Club | <span className="dps">DPSRPK</span>
+            </p>
 
-      <div className="container">
-        <div className="home-content">
-          <p className="top tag">
-            Tech Club | <span className="dps">DPSRPK</span>
-          </p>
+            <h1>
+              turning <span className="text-[#fac71e]">ideas</span>
+              <br />
+              into <span className="text-[#fac71e]">projects</span>
+            </h1>
 
-          <h1>
-            turning <span className="text-[#fac71e]">ideas</span>
-            <br />
-            into <span className="text-[#fac71e]">projects</span>
-          </h1>
+            <p className="description">
+              Whether you’re obsessed with AI, code, robots, or just making things look cool, this
+              is the perfect place to overcommit, under-caffeinate, and accidentally invent the
+              future.
+            </p>
 
-          <p className="description">
-            Whether you’re obsessed with AI, code, robots, or just making things look cool, this is
-            the perfect place to overcommit, under-caffeinate, and accidentally invent the future.
-          </p>
-
-          <div className="button-group">
-            <Link href="/signup" className="btn-1">
-              Join The Discord
-            </Link>
-            <Link href="/signup" className="btn-2">
-              Sign Up
-            </Link>
+            <div className="button-group">
+              <Link href="/signup" className="btn-1">
+                Join The Discord
+              </Link>
+              <Link href="/signup" className="btn-2">
+                Sign Up
+              </Link>
+            </div>
           </div>
         </div>
-      </div>
+      </section>
     </section>
   );
 };
