f626347b2fce1c356e40a256455060f4570fc252
RizDas <dkkrishabh@gmail.com>
2026-01-01 09:50:13 +0530

    tag position fix

--- src/components/home/hero.tsx
+++ src/components/home/hero.tsx
@@ -7,40 +7,39 @@ type HeroProps = React.HTMLAttributes<HTMLElement>;
 
 const Hero = ({ className, ...props }: HeroProps) => {
   return (
-    <>
-      <p className="tag">
-        Tech Club | <span className="dps">DPSRPK</span>
-      </p>
-      <section {...props} className={`home ${className ?? ''}`}>
-        <div className="yellow-circle-top-left" />
-        <div className="yellow-circle-bottom-right" />
+    <section {...props} className={`home ${className ?? ''}`}>
+      <div className="yellow-circle-top-left" />
+      <div className="yellow-circle-bottom-right" />
 
-        <div className="container">
-          <div className="home-content">
-            <h1>
-              turning <span className="text-[#fac71e]">ideas</span>
-              <br />
-              into <span className="text-[#fac71e]">projects</span>
-            </h1>
+      <div className="container">
+        <div className="home-content">
+          <p className="top">
+            Tech Club | <span className="dps">DPSRPK</span>
+          </p>
 
-            <p className="description">
-              Whether you’re obsessed with AI, code, robots, or just making <br /> things look cool,
-              this is the perfect place to overcommit, <br />
-              under-caffeinate, and accidentally invent the future.
-            </p>
+          <h1>
+            turning <span className="text-[#fac71e]">ideas</span>
+            <br />
+            into <span className="text-[#fac71e]">projects</span>
+          </h1>
 
-            <div className="button-group">
-              <Link href="/signup" className="btn-1">
-                Join The Discord
-              </Link>
-              <Link href="/signup" className="btn-2">
-                Sign Up
-              </Link>
-            </div>
+          <p className="description">
+            Whether you’re obsessed with AI, code, robots, or just making <br /> things look cool,
+            this is the perfect place to overcommit, <br />
+            under-caffeinate, and accidentally invent the future.
+          </p>
+
+          <div className="button-group">
+            <Link href="/signup" className="btn-1">
+              Join The Discord
+            </Link>
+            <Link href="/signup" className="btn-2">
+              Sign Up
+            </Link>
           </div>
         </div>
-      </section>
-    </>
+      </div>
+    </section>
   );
 };
 
