0d38b03b273463ba49f99949ce3e218cbd790ddc
Naitik Chattaraj <naitikchattaraj41@gmail.com>
2026-01-04 15:33:27 +0530

    okk

--- public/pass.js
+++
@@ -1,7 +0,0 @@
-const bcrypt = await import('bcrypt').then(mod => mod.default);
-
-const password = 'techclub2526@nophp';
-const hash = '$2b$10$6P9hMTbFDA7FZFwNOK6WqOma5H0uG6eh1QOzEBSgxKgb/PBCpC4LG';
-
-const match = await bcrypt.compare(password, hash);
-console.log('Password match:', match);
